aff400929a9b5fb428df06bc15573c6aaadd7e61
designer (Designer) <no-reply@simplicite.fr>
2022-02-23 19:08:58 +0100

    Order

--- resources/ObjectExternal/DemoJSLib/SCRIPT.js
+++ resources/ObjectExternal/DemoJSLib/SCRIPT.js
@@ -1,34 +1,41 @@
 const DemoJSLib = (() => {
-	function displayForm() {
-		$ui.displayForm(null, 'DemoProduct', $(this).data('id'), { nav: 'add' });
+	const app  = simplicite.session(); // Client lib is referenced in Java code
+	app.info(`Using lib version: ${simplicite.constants.MODULE_VERSION}`);
+
+	const prd = app.getBusinessObject('DemoProduct');
+	const ord = app.getBusinessObject('DemoOrder');
+
+	function product(evt) {
+		$ui.displayForm(null, prd.getName(), $(this).parent().data('item').row_id, { nav: 'add' });
 	}
-
-	function render(params) {
-		const app = simplicite.session();
-		app.info(`Using lib version: ${simplicite.constants.MODULE_VERSION}`);
-
-		const prd = app.getBusinessObject('DemoProduct');
-		prd.search({ demoPrdAvailable: true }).then(list => {
-			const p = $('<ul/>');
-			for (const item of list) {
-				p.append($('<li/>')
-					.append($('<img/>').attr('src', prd.getFieldDocumentURL('demoPrdPicture', item)))
-					.append($('<h1/>').text(item.demoPrdName))
-					.append($('<h2/>').text(item.demoPrdReference))
-					.append($('<p/>').html(item.demoPrdDescription))
-					.data('id', item.row_id).click(displayForm)
-				);
-			}
-			$('#demojslib').append(p);
+	
+	function order(evt) {
+		const prd = $(this).parent().data('item');
+		ord.getForCreate().then(item => {
+			item.demoOrdPrdId = prd.row_id;
+			// Populate other referenced fields
+			for (const f in prd)
+				if (typeof item[`demoOrdPrdId__${f}`] !== "undefined")
+					item[`demoOrdPrdId__${f}`] = prd[f];
+			$ui.displayForm(null, 'DemoOrder', app.constants.DEFAULT_ROW_ID, { nav: 'add', metadata: true, values: item });
 		});
 	}
 
-	return {
-		render: (params) => {
-			$ui.loadScript({
-				url: "https://unpkg.com/simplicite/dist/simplicite.min.js",
-				onload: () => render(params)
-			});
-		}
-	};
+	function render(params) {
+		prd.search({ demoPrdAvailable: true }).then(list => {
+			const p = $('<ul/>');
+			for (const item of list) {
+				p.append($('<li/>').data('item', item)
+					.append($('<img/>').attr('src', prd.getFieldDocumentURL('demoPrdPicture', item)).click(product))
+					.append($('<h1/>').text(item.demoPrdName))
+					.append($('<h2/>').text(item.demoPrdReference))
+					.append($('<p/>').html(item.demoPrdDescription))
+					.append($('<button/>').text('Order !').click(order))
+				);
+			}
+			$('#demojslib').html(p);
+		});
+	}
+
+	return { render: render };
 })();
--- src/com/simplicite/extobjects/DemoJSLib/DemoJSLib.java
+++ src/com/simplicite/extobjects/DemoJSLib/DemoJSLib.java
@@ -2,4 +2,7 @@ package com.simplicite.extobjects.DemoJSLib;
 
 public class DemoJSLib extends com.simplicite.webapp.web.ResponsiveExternalObject {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init(com.simplicite.util.tools.Parameters params) { addSimpliciteClient(); }
 }
