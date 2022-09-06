9169e2aa36d48fa9a91727b6250311e1e78e257e
designer (Designer) <no-reply@simplicite.fr>
2022-09-06 15:31:34 +0200

    Styles

--- src/com/simplicite/extobjects/DemoJSLib/DemoJSLib.java
+++ src/com/simplicite/extobjects/DemoJSLib/DemoJSLib.java
@@ -1,13 +1,8 @@
 package com.simplicite.extobjects.DemoJSLib;
 
-/**
- * Simplicit&eacute;&reg; JS lib page
- */
 public class DemoJSLib extends com.simplicite.webapp.web.ResponsiveExternalObject {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public void init(com.simplicite.util.tools.Parameters params) {
-		addSimpliciteClient();
-	}
+	public void init(com.simplicite.util.tools.Parameters params) { addSimpliciteClient(); }
 }
