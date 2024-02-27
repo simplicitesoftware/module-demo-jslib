5e823a1c818ecfcd8d0bdc6f17281c1e0fcdb0e0
designer (Designer) <simplicite@simplicite.fr>
2024-02-27 10:57:33 +0100

    Modernized JS syntax

--- resources/ObjectExternal/DemoJSLib/SCRIPT.js
+++ resources/ObjectExternal/DemoJSLib/SCRIPT.js
@@ -1,36 +1,39 @@
-var DemoJSLib = DemoJSLib || (() => {
-	const app  = simplicite.session(); // Client lib is referenced in Java code
-	app.info(`Using lib version: ${simplicite.constants.MODULE_VERSION}`);
+//-----------------------------------------------------------
+// Client side JavaScript for JSLib demo page
+//-----------------------------------------------------------
 
-	const prd = app.getBusinessObject('DemoProduct');
-	const ord = app.getBusinessObject('DemoOrder');
+class DemoJSLib {
 
-	function product(evt) {
-		$ui.displayForm(null, prd.getName(), $(this).parent().data('item').row_id, { nav: 'add' });
-	}
-	
-	function order(evt) {
-		const prd = $(this).data('item');
-		ord.getForCreate().then(item => {
-			item.demoOrdPrdId = prd.row_id;
-			// Populate other referenced fields
-			for (const f in prd)
-				if (typeof item[`demoOrdPrdId__${f}`] !== "undefined")
-					item[`demoOrdPrdId__${f}`] = prd[f];
-			$ui.displayForm(null, ord.getName(), app.constants.DEFAULT_ROW_ID, { nav: 'add', metadata: true, values: item });
-		});
-	}
+	static render(params) {
+		const app = simplicite.session(); // Client lib is referenced in Java code
+		app.info(`Using lib version: ${simplicite.constants.MODULE_VERSION}`);
 
-	function render(params) {
+		const prd = app.getBusinessObject('DemoProduct');
+		const ord = app.getBusinessObject('DemoOrder');
+
 		prd.search({ demoPrdAvailable: true }).then(list => {
+			const product = evt => $ui.displayForm(null, prd.getName(), $(evt.target).data('row_id'), { nav: 'add' });
+
+			const order = evt => {
+				const prd = $(evt.target).data('item');
+				ord.getForCreate().then(item => {
+					item.demoOrdPrdId = prd.row_id;
+					// Populate other referenced fields
+					for (const f in prd)
+						if (typeof item[`demoOrdPrdId__${f}`] !== "undefined")
+							item[`demoOrdPrdId__${f}`] = prd[f];
+					$ui.displayForm(null, ord.getName(), app.constants.DEFAULT_ROW_ID, { nav: 'add', metadata: true, values: item });
+				});
+			};
+
 			const products = $('#demojslib').empty().addClass("row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5");
 			let i = 0;
 			for (const item of list) {
 				products.append(
 					$('<div class="col"/>')
 						.append($('<div class="card"/>')
-							.append($('<img/>').attr('src', prd.getFieldDocumentURL('demoPrdPicture', item)).click(product))
 							.append($('<div class="card-body"/>')
+								.append($('<img class="btn"/>').data('row_id', item.row_id).attr('src', prd.getFieldDocumentURL('demoPrdPicture', item)).on('click', product))
 								.append($('<h5 class="card-title"/>').text(item.demoPrdName))
 								.append($('<h6 class="card-subtitle mb2-2 text-muted"/>').text(item.demoPrdReference))
 								.append($('<p class="card-text"/>').html(item.demoPrdDescription))
@@ -42,6 +45,5 @@ var DemoJSLib = DemoJSLib || (() => {
 			}
 		});
 	}
-
-	return { render: render };
-})();
+	
+}
