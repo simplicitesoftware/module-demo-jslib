60e2d17a2f9ba744fec53c8374a151bd8a755da4
designer (Designer) <simplicite@simplicite.fr>
2024-04-22 16:44:56 +0200

    Doc popup

--- resources/ObjectExternal/DemoJSLib/SCRIPT.js
+++ resources/ObjectExternal/DemoJSLib/SCRIPT.js
@@ -2,6 +2,8 @@
 // Client side JavaScript for JSLib demo page
 //-----------------------------------------------------------
 
+/* global $ui, $tools */
+
 class DemoJSLib {
 
 	static render(params) {
@@ -12,16 +14,17 @@ class DemoJSLib {
 		const ord = app.getBusinessObject('DemoOrder');
 
 		prd.search({ demoPrdAvailable: true }).then(list => {
-			const product = evt => $ui.displayForm(null, prd.getName(), $(evt.target).data('row_id'), { nav: 'add' });
+			const productForm = evt => $ui.displayForm(null, prd.getName(), $(evt.target).data('row_id'), { nav: 'add' });
+			const productDoc = evt => $tools.dialog($(evt.target).data('doc'));
 
 			const order = evt => {
-				const prd = $(evt.target).data('item');
+				const itm = $(evt.target).data('item');
 				ord.getForCreate().then(item => {
-					item.demoOrdPrdId = prd.row_id;
+					item.demoOrdPrdId = itm.row_id;
 					// Populate other referenced fields
-					for (const f in prd)
+					for (const f in itm)
 						if (typeof item[`demoOrdPrdId__${f}`] !== "undefined")
-							item[`demoOrdPrdId__${f}`] = prd[f];
+							item[`demoOrdPrdId__${f}`] = itm[f];
 					$ui.displayForm(null, ord.getName(), app.constants.DEFAULT_ROW_ID, { nav: 'add', metadata: true, values: item });
 				});
 			};
@@ -33,10 +36,10 @@ class DemoJSLib {
 					$('<div class="col"/>')
 						.append($('<div class="card"/>')
 							.append($('<div class="card-body"/>')
-								.append($('<img class="btn"/>').data('row_id', item.row_id).attr('src', prd.getFieldDocumentURL('demoPrdPicture', item)).on('click', product))
+								.append($('<img class="btn"/>').data('row_id', item.row_id).attr('src', prd.getFieldDocumentURL('demoPrdPicture', item, true)).on('click', productForm))
 								.append($('<h5 class="card-title"/>').text(item.demoPrdName))
 								.append($('<h6 class="card-subtitle mb2-2 text-muted"/>').text(item.demoPrdReference))
-								.append($('<p class="card-text"/>').html(item.demoPrdDescription))
+								.append($('<p class="card-text"/>').css('cursor', 'pointer').data('doc', { width: '50%', title: item.demoPrdName, content: $('<div class="demojslib-doc"/>').append($('<img/>').attr('src', prd.getFieldDocumentURL('demoPrdPicture', item))).append('<hr/>').append(item.demoPrdDocumentation), closeable: true, moveable: true }).html(item.demoPrdDescription).on('click', productDoc))
 								.append($('<button class="btn btn-primary"/>').text('Order !').data('item', item).on('click', order))
 							)
 						)
